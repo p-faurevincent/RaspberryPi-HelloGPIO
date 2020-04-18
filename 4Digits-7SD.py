caaee65a0908591108abb384e6aca9d91ae0163d
Pascal <faurevincent.p@gmail.com>
2020-04-18 17:07:33 +0200

    fix LED state

--- 4Digits-7SD.py
+++ 4Digits-7SD.py
@@ -21,31 +21,31 @@ GPIO.setup(D4, GPIO.OUT)
 def displayD(n):
   if n == 0:
     print("D1")
-    GPIO.output(D2, False)
-    GPIO.output(D3, False)
-    GPIO.output(D4, False)
-    GPIO.output(D1, True)
+    GPIO.output(D2, True)
+    GPIO.output(D3, True)
+    GPIO.output(D4, True)
+    GPIO.output(D1, False)
 
   if n == 1:
     print("D2")
-    GPIO.output(D1, False)
-    GPIO.output(D3, False)
-    GPIO.output(D4, False)
-    GPIO.output(D2, True)
+    GPIO.output(D1, True)
+    GPIO.output(D3, True)
+    GPIO.output(D4, True)
+    GPIO.output(D2, False)
 
   if n == 2:
     print("D3")
-    GPIO.output(D1, False)
-    GPIO.output(D2, False)
-    GPIO.output(D4, False)
-    GPIO.output(D3, True)
+    GPIO.output(D1, True)
+    GPIO.output(D2, True)
+    GPIO.output(D4, True)
+    GPIO.output(D3, False)
 
   if n == 3:
     print("D4")
-    GPIO.output(D1, False)
-    GPIO.output(D2, False)
-    GPIO.output(D3, False)
-    GPIO.output(D4, True)
+    GPIO.output(D1, True)
+    GPIO.output(D2, True)
+    GPIO.output(D3, True)
+    GPIO.output(D4, False)
 
 
 i = 0
